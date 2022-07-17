da2c409df47d81057c10b782b8d940ad44ee6d89
tylergargula <tyler.gargula@gmail.com>
2022-07-17 14:15:32 -0500

    fix: adjust html elements :qa wq:
    ç
    
    df
    sf
    dsf
    ds
    fds
    OP
    
    :QA
    wq:

--- README.md
+++
@@ -1,4 +0,0 @@
-
-
-![Design preview for the Loopstudios landing page coding challenge](./design/desktop-preview.jpg)
-
--- index.js
+++
@@ -1,5 +0,0 @@
-$("button").click(function() {
-  $("body footer div div div:nth-child(1) a").removeClass("navbar-brand");
-  $(".nav-link").css("color", "none");
-  $("#navbarNavAltMarkup").css("height", "0")
-});
